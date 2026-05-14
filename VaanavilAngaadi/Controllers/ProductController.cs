f9d34629e998c30af2d964ebb8af30545871b1d5
DESKTOP-SL6V6BP\Saravanan S <saravanan@mvtechy.com>
2026-05-15 00:02:58 +0530

    Initial commit

---
+++ VaanavilAngaadi/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VaanavilAngaadi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var products = new[]
+            {
+            new { Id = 1, Name = "Apple", Price = 120 },
+            new { Id = 2, Name = "Orange", Price = 80 }
+        };
+
+            return Ok(products);
+        }
+    }
+}
---
+++ VaanavilAngaadi/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Components;
+using VaanavilAngaadi.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient();
+
+builder.Services.AddScoped(sp =>
+{
+    var navigation = sp.GetRequiredService<NavigationManager>();
+    return new HttpClient
+    {
+        BaseAddress = new Uri(navigation.BaseUri)
+    };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.MapControllers();
+
+app.Run();

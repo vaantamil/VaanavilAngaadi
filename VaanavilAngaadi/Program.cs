12b2273b42f08bc4dab9e7d5b2c9ab02d2b8e798
DESKTOP-SL6V6BP\Saravanan S <saravanan@mvtechy.com>
2026-05-15 00:32:35 +0530

    Error Fixed

--- VaanavilAngaadi/Program.cs
+++ VaanavilAngaadi/Program.cs
@@ -24,11 +24,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    //app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+//app.UseStatusCodePagesWithReExecute("/not-found");
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();
